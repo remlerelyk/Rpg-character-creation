7892f7fc3d786823a63b3ccb19f998d43abef4b3
remlerelyk <kylerelmer@hotmail.com>
2014-01-27 11:39:07 -0800

    View Additions
    Adding stuff to the view

--- Rpg character creation/ViewController.m
+++ Rpg character creation/ViewController.m
@@ -18,6 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [_statDescribeLabel setText:[NSString stringWithFormat:@"You have %i many stat points left", appDelegate.totalStats]];
 }
 
 - (void)didReceiveMemoryWarning
