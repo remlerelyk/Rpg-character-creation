9bf9170d10e7c3aacf3c65da65a356fb8f0062b4
smb123w64gb <smb123w64gb@gmail.com>
2014-01-27 11:38:30 -0800

    Added Extra

---
+++ ViewControllerMain.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerMain.h
+//  Rpg character creation
+//
+//  Created by YOUNG, LUCAS ALLEN on 1/27/14.
+//  Copyright (c) 2014 ELMER, KYLER ROBERT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerMain : UIViewController
+
+@end
---
+++ ViewControllerMain.m
@@ -0,0 +1,38 @@
+//
+//  ViewControllerMain.m
+//  Rpg character creation
+//
+//  Created by YOUNG, LUCAS ALLEN on 1/27/14.
+//  Copyright (c) 2014 ELMER, KYLER ROBERT. All rights reserved.
+//
+
+#import "ViewControllerMain.h"
+
+@interface ViewControllerMain ()
+
+@end
+
+@implementation ViewControllerMain
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
