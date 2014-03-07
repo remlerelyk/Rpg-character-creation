dffbbc43164b0f8a09fad5ec91f62f6eab2f6fd7
smb123w64gb <smb123w64gb@gmail.com>
2014-03-07 11:19:35 -0800

    I sink u

--- ViewControllerMain.m
+++ ViewControllerMain.m
@@ -31,6 +31,15 @@
 //Start Here
 - (void)viewDidLoad
 {
+    
+    _Animation = [[UIImageView alloc] initWithFrame:CGRectMake(240, 24, 48, 48)];
+    [self.view addSubview: _Animation];
+    _Animation.image = [UIImage imageNamed:@"Male.png"];
+    
+    _baddy = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, 64, 64)];
+    [self.view addSubview: _baddy];
+    
+    
     appDelegate = [[UIApplication sharedApplication] delegate];
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
@@ -40,13 +49,41 @@
     
     _showDataArray = [[NSMutableArray alloc] initWithObjects:
                       data, nil];
-    
+    _hit = [[NSBundle mainBundle] pathForResource:@"hit" ofType:@"mp3"];
+    _death = [[NSBundle mainBundle] pathForResource:@"death" ofType:@"mp3"];
+    _uwin = [[NSBundle mainBundle] pathForResource:@"ff7" ofType:@"mp3"];
+    _cure = [[NSBundle mainBundle] pathForResource:@"cure" ofType:@"mp3"];
     if(appDelegate.isMusic){
     _audioPath = [[NSBundle mainBundle] pathForResource:@"ff7bat" ofType:@"mp3"];
     _audioURL = [NSURL fileURLWithPath:_audioPath];
     appDelegate.music =[[AVAudioPlayer alloc] initWithContentsOfURL:_audioURL error:nil];
     [appDelegate.music play];
         appDelegate.isMusic = FALSE;
+        
+        
+
+
+        
+        _mWalk = [[NSArray alloc]initWithArray:[NSArray arrayWithObjects:[UIImage imageNamed:@"mf1.gif"],[UIImage imageNamed:@"mf2.gif"],[UIImage imageNamed:@"mf3.gif"],[UIImage imageNamed:@"mf4.gif"], nil]];
+        _mWin = [[NSArray alloc] initWithArray:[NSArray arrayWithObjects:[UIImage imageNamed:@"mf2.gif"],[UIImage imageNamed:@"mw.gif"], nil]];
+        _fWalk = [[NSArray alloc]initWithArray:[NSArray arrayWithObjects:[UIImage imageNamed:@"ff1.gif"],[UIImage imageNamed:@"ff2.gif"],[UIImage imageNamed:@"ff3.gif"],[UIImage imageNamed:@"ff4.gif"], nil]];
+        _fWin = [[NSArray alloc] initWithArray:[NSArray arrayWithObjects:[UIImage imageNamed:@"ff2.gif"],[UIImage imageNamed:@"fw.gif"], nil]];
+        _buttonPress = TRUE;
+        [super viewDidLoad];
+        [_baddy setImage:[UIImage imageNamed:@"baddy.gif"]];
+        
+        
+        
+        if([appDelegate.Player isMale]==TRUE){
+            _uWalk = [[NSArray alloc]initWithArray:_mWalk];
+            _uWin = [[NSArray alloc]initWithArray:_mWin];
+            _uIdle = [UIImage imageNamed:@"mf2.gif"];
+        }else{
+            _uWalk = [[NSArray alloc]initWithArray:_fWalk];
+            _uWin = [[NSArray alloc]initWithArray:_fWin];
+            _uIdle = [UIImage imageNamed:@"ff2.gif"];
+        }
+        [_Animation setImage:_uIdle];
     }
 }
 -(void)viewDidAppear:(BOOL)animated
@@ -97,4 +134,98 @@
         destViewController.currentHealth = _currentHealth;
     }
 }*/
+- (IBAction)Attack:(UIButton *)sender {
+    if (_buttonPress) {
+        _buttonPress = FALSE;
+        [_Animation setAnimationImages:_uWalk];
+        _Animation.animationDuration = 1.0f;
+        [_Animation startAnimating];
+        [UIView animateWithDuration:1.0 animations:^{
+            _Animation.frame = CGRectMake(240,20,48,48);
+            _Animation.frame = CGRectMake(64,20,48,48);
+        }completion:^(BOOL finished) {
+            if (finished){
+                _Animation.transform = CGAffineTransformMakeScale(-1, 1);
+                _audioSFX = [NSURL fileURLWithPath:_hit];
+                appDelegate.sfx =[[AVAudioPlayer alloc] initWithContentsOfURL:_audioSFX error:nil];
+                [appDelegate.sfx play];
+                [_Animation stopAnimating];
+                _Animation.animationDuration = 0.5f;
+                [_Animation startAnimating];
+                [UIView animateWithDuration:0.5 animations:^{
+                    _Animation.frame = CGRectMake(64,20,48,48);
+                    _Animation.frame = CGRectMake(240,20,48,48);
+                }completion:^(BOOL Done){if (Done){
+                    _Animation.transform = CGAffineTransformMakeScale(1, 1);
+
+                    
+                        
+                        
+                        _Temp = [_baddy image];
+                        [UIView animateWithDuration:0.1 animations:^{
+                            _baddy.alpha = 1.0f;
+                            _baddy.alpha = 0.0f;
+                            
+                        }completion:^(BOOL badAttack){if (badAttack){
+                            [UIView animateWithDuration:0.1 animations:^{
+                                _baddy.alpha = 0.0f;
+                                _baddy.alpha = 1.0f;
+                                
+                                
+                                
+                            }completion:^(BOOL ATTACK){if (ATTACK){
+                                [UIView animateWithDuration:0.7 animations:^{
+                                    _audioSFX = [NSURL fileURLWithPath:_hit];
+                                    appDelegate.sfx =[[AVAudioPlayer alloc] initWithContentsOfURL:_audioSFX error:nil];
+                                    [appDelegate.sfx play];
+                                }completion:^(BOOL ATTACK2){if (ATTACK2){
+                                    [UIView animateWithDuration:1.0 animations:^{
+                                        [_Animation stopAnimating];
+                                        [_Animation setAnimationImages:[NSArray arrayWithObject:[UIImage imageNamed:@"mh.gif"
+                                                                                                 ]]];
+                                        
+                                        
+                                        
+                                    }completion:^(BOOL finished) {if(finished){
+                                        [_Animation setAnimationImages:[NSArray arrayWithObject:_uIdle]];
+                                        _buttonPress = TRUE;
+                                    }
+                                    }];
+                                }}];
+                            }}];}
+                        }];}
+                    [_Animation stopAnimating];
+                    [_Animation setImage:_uIdle];
+                    
+                }];
+                
+            }}];
+    }
+}
+- (UIImage *)applyColor:(UIColor *)color toImage:(UIImage*)toImage{
+    UIGraphicsBeginImageContextWithOptions(toImage.size, NO, toImage.scale);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextTranslateCTM(context, 0, toImage.size.height);
+    CGContextScaleCTM(context, 1.0, -1.0);
+    
+    CGContextSetBlendMode(context, kCGBlendModeNormal);
+    CGRect rect = CGRectMake(0, 0, toImage.size.width, toImage.size.height);
+    //CGContextDrawImage(context, rect, img.CGImage);
+    
+    // Create gradient
+    NSArray *colors = [NSArray arrayWithObjects:(id)color.CGColor, (id)color.CGColor, nil];
+    CGColorSpaceRef space = CGColorSpaceCreateDeviceRGB();
+    CGGradientRef gradient = CGGradientCreateWithColors(space, (__bridge CFArrayRef)colors, NULL);
+    
+    // Apply gradient
+    CGContextClipToMask(context, rect, toImage.CGImage);
+    CGContextDrawLinearGradient(context, gradient, CGPointMake(0,0), CGPointMake(0, toImage.size.height), 0);
+    UIImage *coloredImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    CGGradientRelease(gradient);
+    CGColorSpaceRelease(space);
+    
+    return coloredImage;
+}
 @end
